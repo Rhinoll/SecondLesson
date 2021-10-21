a39da7e79da1e60f730fb62296a705db85b5df73
Andrew <zenit.yfon@gmail.com>
2021-10-21 19:27:28 +0300

    1.11 делается

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,73 +1,64 @@
 package com.company;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        /*
-        int x = 0;
-        while (x > 0 || x != 3 || x == 5) {
-            //здесь свой код
-        }
-
-        for (int i = 0; i < 10; i++) {
-            if (i == 5) {
-                continue;
-                break; //выход из цикла
+    public static void reverser(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n / 2; ++j) {
+                int temp = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = matrix[i][j];
+                matrix[i][j] = temp;
             }
-            //здесь дальше свой код
-        }
-
-
-        int[] a = new int[5]; //массивы 0 0 0 0 0
-        int b[] = new int[10];// нежелательный вариант
-        //заполнение происходит только после инициализации
-        for (int i = 0; i < b.length; i++) { //обычный цикл
-            System.out.print(b[i] + " ");
-        }
-        for (int i : b) System.out.println(i + " "); //цикл foreach
-
-        int[] c = {6, 4, 2, 900, -9333};
-        c[2] = 45; //тоже вариант заполнения
-
-        //двумерный массив
-        int [][] y = new int [4][4];//первое число - строки, вторая - стобцы
-        for (int i = 0; i < y[i].length;i++) {
-            for (int j = 0; j < y[j].length; j++) {
-                System.out.print(y[i][j] + " ");
-            }
-            System.out.println();
-        ArrayList aL = new ArrayList<>(); //20 элементов по умолчанию. Саморасширяющий массив;
-        aL.add(0);
-        aL.add("qwerty");
-        int v = (int) aL.get(2); //невозможно вытащить строку в виде int
-
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(123);
-        //list.add("qwerty"); метод защиты от случайного попадания
-
-        int g = 0;
-        int h;
-        if (g == 100) {
-            h = 900;
-        }
-        g = 1000;
-
-
-        Scanner scan = new Scanner(System.in);
-        for (int i = 0; i < 10; i++) {
-            b[i] = scan.nextInt();
-            }
-            for (int i: b)
-             */
-
-
         }
     }
 
+    public static void transposer(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
 
+    public static void rotate90(int[][] matrix) {
+        transposer(matrix);
+        reverser(matrix);
+    }
+
+    public static void print(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                System.out.print(matrix[i][j]);
+                System.out.print(' ');
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        int[][] matrix = new int[a][b];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = scan.nextInt();
+            }
+        }
+        System.out.println(b + " " + a);
+        rotate90(matrix);
+        print(matrix);
+    }
+}
 
 
 
