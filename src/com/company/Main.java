ef93f95305f8bf9691c511d00b00bc861f2e07a6
Andrew <zenit.yfon@gmail.com>
2021-10-22 14:25:19 +0300

    Test done, 79 percents

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,62 +1,26 @@
 package com.company;
-
 import java.util.Scanner;
 
 public class Main {
-
-    public static void reverser(int[][] matrix) {
-        int n = matrix.length;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n / 2; ++j) {
-                int temp = matrix[i][n - j - 1];
-                matrix[i][n - j - 1] = matrix[i][j];
-                matrix[i][j] = temp;
-            }
-        }
-    }
-
-    public static void transposer(int[][] matrix) {
-        int n = matrix.length;
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
-            }
-        }
-    }
-
-    public static void rotate90(int[][] matrix) {
-        transposer(matrix);
-        reverser(matrix);
-    }
-
-    public static void print(int[][] matrix) {
-        int n = matrix.length;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                System.out.print(matrix[i][j]);
-                System.out.print(' ');
-            }
-            System.out.println();
-        }
-    }
-
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-
-        int[][] matrix = new int[a][b];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = scan.nextInt();
+        short check = -1;
+        int n = scan.nextInt();
+        int [] array = new int[n];
+        for (int i = 0; i < n; i++){
+            array[i] = scan.nextInt();
+        }
+        int x = scan.nextInt();
+        for (int i = 0; i < n; i++){
+            if (array[i]==x){
+                System.out.print(i+1 + " ");
+                check = 0;
             }
         }
-        System.out.println(b + " " + a);
-        rotate90(matrix);
-        print(matrix);
+        if (check == -1) {
+            System.out.println(check);
+        }
     }
 }
 
---
+++ src/com/company/testSams.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class testSams {
+    public static void main (String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        System.out.println(x - (double)x);
+    }
+}
